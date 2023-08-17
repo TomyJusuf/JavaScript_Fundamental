d0d52f031764794e9b413429c9aec116ae2135e0
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2023-08-17 11:25:05 +0200

    Add files via upload

---
+++ JavaScript_Fundaments/DOM/1_DOM_generalConcept.js
@@ -0,0 +1,23 @@
+// similat to css
+
+// Select the element or group of elements the you we  want to affect
+// Decide the effect we want to apply to the selection
+
+// many different ways
+
+document.body.style.backgroundColor = 'blue';
+document.body.style.color = 'yellow';
+document.getElementById('btn').style.color = 'red';
+
+// assing to variable
+
+const element = document.getElementById('element');
+// do something
+
+document.querySelector('element'); // do something
+
+// window.object
+// console.log(window);
+
+// return a node object or a node list, which is an array like object
+// https://www.w3schools.com/jsref/dom_obj_html_nodelist.asp#:~:text=A%20NodeList%20is%20a%20collection,accessed%20by%20their%20index%20number.
